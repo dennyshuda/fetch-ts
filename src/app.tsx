fed6595341fd3182ce25cf08c011a787e6af0b92
Denny Setyawan Huda <dennshgit@gmail.com>
2022-12-13 19:01:15 +0700

    feat: add routes

--- src/app.tsx
+++ src/app.tsx
@@ -1,19 +1,19 @@
 import { Link, Route, Routes } from "react-router-dom";
-import { Book } from "./pages/book";
 import { Home } from "./pages/home";
 import { Post } from "./pages/post";
+import { Posts } from "./pages/posts";
 
 function App() {
   return (
     <div className="App">
       <nav>
         <Link to={"/"}>Home</Link>
-        <Link to={"/books"}>Book</Link>
+        <Link to={"/posts"}>Posts</Link>
       </nav>
 
       <Routes>
         <Route path="/" element={<Home />}></Route>
-        <Route path="/books" element={<Book />}></Route>
+        <Route path="/posts" element={<Posts />}></Route>
         <Route path="/post/:id" element={<Post />}></Route>
       </Routes>
     </div>
